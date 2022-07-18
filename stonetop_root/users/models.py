d1979ff704ca1e23d91d3a03ef59a426cb528c65
Scott Clements <clemencyoflee@gmail.com>
2022-07-18 13:34:05 -0700

    Initial commit with custom user model.

---
+++ stonetop_root/users/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+'''
+class CustUserManager(BaseUserManager):
+    """
+    CustUserManager is the manager used when creating new users.
+    """
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        if not email:
+            raise ValueError('Email address is required.')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            is_active=True,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
+'''
+
+class TableTopUser(AbstractUser):
+    """
+    Create a custom user so that an email can be used in place of an email
+    and/or other customizations can take place with the users.
+    """
+    email = models.EmailField(_('email address'), unique=True)
+    
+    def __str__(self):
+        return f"{self.username}"
