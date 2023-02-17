a13b86027c74fbc8c004352f97aa8e6178fa230c
Scott Clements <clemencyoflee@gmail.com>
2023-02-17 14:45:52 -0800

    Fixed an issue with the GM being capitalized for the account page and added tests for the account page.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from campaign.tests.test_views.base_views import BaseViewsTestClass
+
+User = get_user_model()
+
+
+class UserAccountPageTests(BaseViewsTestClass):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser = User.objects.create(
+            username='testuser',
+            email='player1@test.com',
+            password='109wdmgbowei8idj',
+        )
+        testuser.save()
+        cls.testuser = testuser
+
+    def test_user_account_page_uses_tabletopuser_detail_template(self):
+        self.login_user(self.testuser)
+        
+        response = self.client.get(f'/users/{self.testuser.pk}/')
+        self.assertTemplateUsed(response, 'users/tabletopuser_detail.html')
+
+    def test_user_account_page_redirects_if_not_logged_in(self):
+        response = self.client.get(f"/users/{self.testuser.pk}/")
+        self.assertRedirects(response, f'/login/?next=/users/{self.testuser.pk}/')
+
+
